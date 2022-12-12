9c7a80c355f0d936d7e2585cec42efb94575255c
GrapeHuang <oneheed@gmail.com>
2022-12-12 23:19:16 +0800

    Add ex00226. Invert Binary Tree

---
+++ LeetCode.Core/TreeNode.cs
@@ -0,0 +1,49 @@
+﻿namespace LeetCode.Core
+{
+    //Definition for a binary tree node.
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        public TreeNode(int[] ints)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            var result = new List<string>();
+            result.Add(this.val.ToString());
+            Append(result, this);
+
+            return $"[{string.Join(",", result)}]";
+        }
+
+        private void Append(List<string> result, TreeNode node)
+        {
+            if (node == null)
+                return;
+
+            if (node.left != null || node.right != null)
+            {
+                var leftValue = node.left == null ? "null" : node.left.val.ToString();
+                var rightValue = node.right == null ? "null" : node.right.val.ToString();
+
+                result.Add(leftValue);
+                result.Add(rightValue);
+
+                Append(result, node.left);
+                Append(result, node.right);
+            }
+        }
+    }
+}
---
+++ ex00226. Invert Binary Tree/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LeetCode.Core;
+
+var solution = new Solution();
+
+var input1 = new TreeNode(4, new(2, new(1), new(3)), new(7, new(6), new(9)));
+var output1 = solution.InvertTree(input1);
+Console.WriteLine(output1.ToString()); // [4,7,2,9,6,3,1]
+
+//var input2 = new int[] { 1, 1, 1, 1, 1 };
+//var output2 = solution.RunningSum(input2);
+//Console.WriteLine(string.Join(",", output2)); // [1,2,3,4,5]
+
+//var input3 = new int[] { 3, 1, 2, 10, 1 };
+//var output3 = solution.RunningSum(input3);
+//Console.WriteLine(string.Join(",", output3)); // [3,4,6,16,17]
+
+
+public class Solution
+{
+    public TreeNode InvertTree(TreeNode root)
+    {
+        if (root == null)
+            return root;
+
+        if (root.left != null || root.right != null)
+        {
+            var temp = root.left;
+            root.left = root.right;
+            root.right = temp;
+
+            InvertTree(root.left);
+            InvertTree(root.right);
+        }
+
+        return root;
+    }
+}
+
