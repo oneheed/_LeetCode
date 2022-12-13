3a4d4d100644be720d1ba4e739f4b75bd8aa7f83
GrapeHuang <oneheed@gmail.com>
2022-12-13 22:50:33 +0800

    Add ex00102. Binary Tree Level Order Traversal and ex0098. Validate Binary Search Treee

---
+++ ex00102. Binary Tree Level Order Traversal/Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LeetCode.Core;
+
+var solution = new Solution();
+
+var input1 = new TreeNode(3, new(9), new(20, new(15), new(7)));
+var output1 = solution.LevelOrder(input1);
+Console.WriteLine(output1.ToString()); // [4,7,2,9,6,3,1]
+
+//var input2 = new int[] { 1, 1, 1, 1, 1 };
+//var output2 = solution.RunningSum(input2);
+//Console.WriteLine(string.Join(",", output2)); // [1,2,3,4,5]
+
+//var input3 = new int[] { 3, 1, 2, 10, 1 };
+//var output3 = solution.RunningSum(input3);
+//Console.WriteLine(string.Join(",", output3)); // [3,4,6,16,17]
+
+
+public class Solution
+{
+    public IList<IList<int>> LevelOrder(TreeNode root)
+    {
+        var resut = new List<IList<int>>();
+
+        resut.Add(new List<int> { root.val });
+
+
+        while (root.left != null)
+        {
+            var temp = new List<int>();
+
+            if (root.left != null)
+                temp.Add(root.left.val);
+
+            if (root.right != null)
+                temp.Add(root.right.val);
+
+            resut.Add(temp);
+
+            root.left = root.left.left;
+        }
+
+        while (root.right != null)
+        {
+            var temp = new List<int>();
+
+            if (root.left != null)
+                temp.Add(root.left.val);
+
+            if (root.right != null)
+                temp.Add(root.right.val);
+
+            resut.Add(temp);
+
+            root.right = root.right.right;
+        }
+
+        return resut;
+    }
+}
+
---
+++ ex0098. Validate Binary Search Treee/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LeetCode.Core;
+
+var solution = new Solution();
+
+var input1 = new TreeNode(2, new(1), new(3));
+var output1 = solution.IsValidBST(input1);
+Console.WriteLine(output1.ToString()); // [4,7,2,9,6,3,1]
+
+var input2 = new TreeNode(5, new(1), new(4, new(3), new(6)));
+var output2 = solution.IsValidBST(input2);
+Console.WriteLine(string.Join(",", output2)); // [1,2,3,4,5]
+
+var input3 = new TreeNode(5, new(4), new(6, new(3), new(7)));
+var output3 = solution.IsValidBST(input3);
+Console.WriteLine(string.Join(",", output3)); // [3,4,6,16,17]
+
+
+public class Solution
+{
+    private TreeNode prev = null;
+
+    public bool IsValidBST(TreeNode root)
+    {
+        if (root == null)
+        {
+            return true;
+        }
+
+        if (!IsValidBST(root.left) || prev != null && prev.val >= root.val)
+        {
+            return false;
+        }
+
+        prev = root;
+
+        return IsValidBST(root.right);
+    }
+}
+
