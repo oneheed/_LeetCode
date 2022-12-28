42f5846bb17554d0627da4b8d1bb803a246aba0c
GrapeHuang <oneheed@gmail.com>
2022-12-28 09:14:02 +0800

    Add ex00011 and ex00042

---
+++ ex00011. Container With Most Water/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+var solution = new Solution();
+
+var input1 = new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+var output1 = solution.MaxArea(input1);
+Console.WriteLine(output1); // 49
+
+var input2 = new int[] { 1, 1 };
+var output2 = solution.MaxArea(input2);
+Console.WriteLine(output2); // 1
+
+
+public class Solution
+{
+    public int MaxArea(int[] height)
+    {
+        var left = 0;
+        var right = height.Length - 1;
+        var result = int.MinValue;
+
+        while (left < right)
+        {
+            var temp = Math.Min(height[left], height[right]) * (right - left);
+            if (temp > result)
+            {
+                result = temp;
+            }
+
+            if (height[left] > height[right])
+                right--;
+            else
+                left++;
+        }
+
+        return result;
+    }
+}
---
+++ ex00042. Trapping Rain Water/Program.cs
@@ -0,0 +1,65 @@
+﻿// See https://aka.ms/new-console-template for more information
+var solution = new Solution();
+
+var input1 = new int[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+var output1 = solution.Trap(input1);
+Console.WriteLine(output1); // 6
+
+var input2 = new int[] { 4, 2, 0, 3, 2, 5 };
+var output2 = solution.Trap(input2);
+Console.WriteLine(output2); // 9
+
+var input3 = new int[] { 4, 2, 3 };
+var output3 = solution.Trap(input3);
+Console.WriteLine(output3); // 1
+
+public class Solution
+{
+    public int Trap(int[] height)
+    {
+        var result = 0;
+
+        var left = 0;
+
+        while (height[left] == 0)
+            left++;
+
+        while (left < height.Length - 2)
+        {
+            var diff = height[left + 1];
+            var right = left + 2;
+            var temp = Math.Min(height[left], height[right]) * (right - left - 1) - diff;
+
+            while (temp < 0)
+            {
+                diff += height[right];
+                right++;
+                temp = Math.Min(height[left], height[right]) * (right - left - 1) - diff;
+            }
+
+            if (right == height.Length - 1 && temp > 0)
+            {
+                result += temp;
+                break;
+            }
+
+            while (right < height.Length - 1)
+            {
+                diff += height[right];
+                right++;
+
+                var temp2 = Math.Min(height[left], height[right]) * (right - left - 1) - diff;
+                if (temp > temp2)
+                {
+                    result += temp;
+                    left = right - 1;
+                    break;
+                }
+
+                temp = temp2;
+            }
+        }
+
+        return result;
+    }
+}
