d27204ed1217ccd42527bcad3edbd7a8918202fc
GrapeHuang <oneheed@gmail.com>
2023-02-11 23:44:10 +0800

    ex00045. Jump Game II
    ex01162. As Far from Land as Possible
    ex01470. Shuffle the Array

---
+++ ex01162. As Far from Land as Possible/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var solution = new Solution();
+
+var grid1 = new int[][] { new[] { 1, 0, 1 }, new[] { 0, 0, 0 }, new[] { 1, 0, 1 } };
+var output1 = solution.MaxDistance(grid1);
+Console.WriteLine(output1.ToString()); // 2
+
+public class Solution
+{
+    public int MaxDistance(int[][] grid)
+    {
+        var queue = new Queue<(int x, int y)>();
+        var n = grid.Length;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (grid[i][j] == 1)
+                {
+                    queue.Enqueue((i, j));
+                }
+            }
+        }
+
+        if (!queue.Any() || queue.Count == n * n)
+            return -1;
+
+        var dir = new int[][] { new[] { 0, -1 }, new[] { 0, 1 }, new[] { -1, 0 }, new[] { 1, 0 } };
+        var dis = -1;
+        while (queue.Count > 0)
+        {
+            var count = queue.Count;
+            for (int i = 0; i < count; i++)
+            {
+                var loc = queue.Dequeue();
+
+                for (int j = 0; j < dir.Length; j++)
+                {
+                    var x = loc.x + dir[j][0];
+                    var y = loc.y + dir[j][1];
+                    if (x >= 0 && x < n &&
+                        y >= 0 && y < n &&
+                        grid[x][y] == 0)
+                    {
+                        grid[x][y] = 1;
+                        queue.Enqueue((x, y));
+                    }
+                }
+            }
+
+            dis++;
+        }
+
+        return dis;
+    }
+}
---
+++ ex01470. Shuffle the Array/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var solution = new Solution();
+
+var nums1 = new int[] { 2, 5, 1, 3, 4, 7 };
+var n1 = 3;
+var output1 = solution.Shuffle(nums1, n1);
+Console.WriteLine(string.Join(",", nums1)); // [2,3,5,4,1,7] 0 3 1 4 2 5 
+
+public class Solution
+{
+    public int[] Shuffle(int[] nums, int n)
+    {
+        var res = new int[nums.Length];
+        var diff = nums.Length / n;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < diff; j++)
+            {
+
+                //Console.WriteLine($"{i} {j} {i + j * n}");
+                //Console.WriteLine(i * diff + j);
+
+                res[i * diff + j] = nums[i + j * n];
+            }
+        }
+
+        return res;
+    }
+}
